2078aa231cab966306add9cb22406012159fbdf3
Jen Hamon <jhamon@pinecone.io>
2024-05-31 09:32:02 -0400

    Whitespace change

--- codegen/build-oas.sh
+++ codegen/build-oas.sh
@@ -10,6 +10,6 @@ mkdir $outdir
 
 docker run --rm -v $(pwd):/workspace openapitools/openapi-generator-cli:v7.6.0 generate \
 	--input-spec /workspace/codegen/apis/_build/2024-07/control_2024-07.oas.yaml \
-    --generator-name rust \
+	--generator-name rust \
 	--output /workspace/$outdir \
 	--additional-properties "packageVersion=0.0.1"
