28b3f5619332c651cce2d17491452b72e2b823b2
Jen Hamon <jhamon@pinecone.io>
2024-06-03 11:58:24 -0400

    Fix tests

--- pinecone_sdk/src/control/list_indexes.rs
+++ pinecone_sdk/src/control/list_indexes.rs
@@ -1,8 +1,8 @@
 use crate::pinecone::Pinecone;
 use openapi::apis::manage_indexes_api;
-use openapi::models;
-use openapi::apis::Error;
 use openapi::apis::manage_indexes_api::ListIndexesError;
+use openapi::apis::Error;
+use openapi::models;
 
 impl Pinecone {
     pub async fn list_indexes(&self) -> Result<models::IndexList, Error<ListIndexesError>> {
@@ -15,9 +15,16 @@ impl Pinecone {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tokio;
     use mockito::mock;
+    use tokio;
+    use crate::control::list_indexes::models::index_model::Metric;
+    use crate::config::Config;
     use openapi::models::IndexList;
+    use openapi::apis::configuration::Configuration;
+    use openapi::apis::configuration::ApiKey;
+    use openapi::models::IndexModel;
+
+
 
     #[tokio::test]
     async fn test_list_indexes() {
@@ -25,7 +32,8 @@ mod tests {
         let _m = mock("GET", "/indexes")
             .with_status(200)
             .with_header("content-type", "application/json")
-            .with_body(r#"
+            .with_body(
+                r#"
                 {
                     "indexes": [
                         {
@@ -52,22 +60,13 @@ mod tests {
                         }
                     ]
                 }
-            "#)
+            "#,
+            )
             .create();
 
         // Construct Pinecone instance with the mock server URL
         let api_key = "test_api_key".to_string();
-        let config = Config::new(api_key.clone());
-        let openapi_config = Configuration {
-            base_path: mockito::server_url(), // Use mock server URL
-            user_agent: Some("pinecone-rust-client".to_string()),
-            api_key: Some(ApiKey {
-                prefix: None,
-                key: api_key,
-            }),
-            ..Default::default()
-        };
-        let pinecone = Pinecone { config, openapi_config };
+        let pinecone = Pinecone::new(api_key, Some(mockito::server_url()));
 
         // Call list_indexes and verify the result
         let result = pinecone.list_indexes().await;
@@ -77,9 +76,23 @@ mod tests {
                 let expected = IndexList {
                     // name: String, dimension: i32, metric: Metric, host: String, spec: models::IndexModelSpec, status: models::IndexModelStatus)
                     indexes: Some(vec![
-                                IndexModel::new("index1".to_string(), 1536, Metric::Cosine, "host1".to_string(), models::IndexModelSpec::default(), models::IndexModelStatus::default()),
-                                IndexModel::new("index2".to_string(), 1536, Metric::Cosine, "host2".to_string(), models::IndexModelSpec::default(), models::IndexModelStatus::default()),
-                            ]),
+                        IndexModel::new(
+                            "index1".to_string(),
+                            1536,
+                            Metric::Cosine,
+                            "host1".to_string(),
+                            models::IndexModelSpec::default(),
+                            models::IndexModelStatus::default(),
+                        ),
+                        IndexModel::new(
+                            "index2".to_string(),
+                            1536,
+                            Metric::Cosine,
+                            "host2".to_string(),
+                            models::IndexModelSpec::default(),
+                            models::IndexModelStatus::default(),
+                        ),
+                    ]),
                 };
                 assert_eq!(index_list, expected);
             }
--- pinecone_sdk/src/pinecone.rs
+++ pinecone_sdk/src/pinecone.rs
@@ -1,19 +1,19 @@
 use crate::config::Config;
-use openapi::apis::configuration::Configuration;
 use openapi::apis::configuration::ApiKey;
+use openapi::apis::configuration::Configuration;
 
 #[derive(Debug, Clone)]
 pub struct Pinecone {
     config: Config,
-    openapi_config: Configuration
+    openapi_config: Configuration,
 }
 
 impl Pinecone {
-    pub fn new(api_key: String) -> Self {
+    pub fn new(api_key: String, control_plane_host: Option<String>) -> Self {
         let config = Config::new(api_key.clone());
-        
+
         let openapi_config = Configuration {
-            base_path: "https://api.pinecone.io".to_string(),
+            base_path: control_plane_host.unwrap_or("https://api.pinecone.io".to_string()),
             user_agent: Some("pinecone-rust-client".to_string()),
             api_key: Some(ApiKey {
                 prefix: None,
@@ -22,7 +22,10 @@ impl Pinecone {
             ..Default::default()
         };
 
-        Pinecone { config, openapi_config }
+        Pinecone {
+            config,
+            openapi_config,
+        }
     }
 
     pub fn openapi_config(&self) -> &Configuration {
