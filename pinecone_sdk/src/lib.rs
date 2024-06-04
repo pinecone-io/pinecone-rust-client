e6d95374a2553d3aff01d032fad40a9443908ad9
Erica Wang <erica.py.wang@gmail.com>
2024-06-04 16:15:14 -0400

    added source tag cleaning

--- pinecone_sdk/src/lib.rs
+++ pinecone_sdk/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod config;
 pub mod control;
 pub mod pinecone;
+pub mod utils;
---
+++ pinecone_sdk/src/utils/user_agent.rs
@@ -0,0 +1,30 @@
+use regex::Regex;
+
+// Normalizes the source tag
+fn build_source_tag(source_tag: String) -> String {
+    // 1. Lowercase
+    // 2. Limit charset to [a-z0-9_ ]
+    // 3. Trim left/right empty space
+    // 4. Condence multiple spaces to one, and replace with underscore
+
+    let re = Regex::new(r"[^a-z0-9_ ]").unwrap();
+    let lowercase_tag = source_tag.to_lowercase();
+    let tag = re.replace_all(&lowercase_tag, "");
+    return tag.trim()
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .collect::<Vec<&str>>()
+        .join("_");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio;
+
+    #[tokio::test]
+    async fn test_build_source_tag() {
+        let source_tag = "    Hello   World!! ".to_string();
+        assert_eq!(build_source_tag(source_tag), "hello_world");
+    }
+}
