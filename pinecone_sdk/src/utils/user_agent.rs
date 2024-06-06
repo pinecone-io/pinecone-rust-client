2c51456cfdafdf72d1cf26d1147c16aeb97a0553
Erica Wang <erica.py.wang@gmail.com>
2024-06-06 13:20:05 -0400

    edit user agent string

--- pinecone_sdk/src/utils/user_agent.rs
+++ pinecone_sdk/src/utils/user_agent.rs
@@ -20,7 +20,7 @@ fn build_source_tag(source_tag: &String) -> String {
 
 // Gets user agent string
 pub fn get_user_agent(config: &Config) -> String {
-    let mut user_agent = format!("lang=rust/{}; pinecone-rust-client={}", "0.1.0", "0.1.0");
+    let mut user_agent = format!("lang=rust; pinecone-rust-client={}", "0.1.0");
     if let Some(source_tag) = &config.source_tag {
         user_agent.push_str(&format!("; source_tag={}", build_source_tag(source_tag)));
     }
@@ -47,12 +47,12 @@ mod tests {
     #[tokio::test]
     async fn test_no_source_tag() {
         let config = Config::new("api".to_string(), None);
-        assert_eq!(get_user_agent(&config), "lang=rust/0.1.0; pinecone-rust-client=0.1.0");
+        assert_eq!(get_user_agent(&config), "lang=rust; pinecone-rust-client=0.1.0");
     }
 
     #[tokio::test]
     async fn test_with_source_tag() {
         let config = Config::new("api".to_string(), Some("Tag".to_string()));
-        assert_eq!(get_user_agent(&config), "lang=rust/0.1.0; pinecone-rust-client=0.1.0; source_tag=tag");
+        assert_eq!(get_user_agent(&config), "lang=rust; pinecone-rust-client=0.1.0; source_tag=tag");
     }
 }
