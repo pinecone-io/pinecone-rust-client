d9cf6b6ff475a6b665e4dc18efadfe6666ef26cf
Emily Yu <emily.y@pinecone.io>
2024-06-11 16:15:32 -0400

    Update missed test cases for return type

--- pinecone_sdk/tests/integration_test.rs
+++ pinecone_sdk/tests/integration_test.rs
@@ -1,9 +1,13 @@
 use pinecone_sdk::pinecone::Pinecone;
+use pinecone_sdk::utils::errors::PineconeError;
 
 #[tokio::test]
-async fn test_list_serverless_index_env() {
+async fn test_list_index_env() -> Result<(), PineconeError> {
     let pinecone = Pinecone::new(None, None, None, None).unwrap();
-    let list_response = pinecone.list_indexes().await;
+    let _ = pinecone
+        .list_indexes()
+        .await
+        .expect("Failed to list indexes");
 
-    assert!(list_response.is_ok());
+    Ok(())
 }
