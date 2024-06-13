cadf7b2c4b73c76104e3fcc0fec68d0108705f2e
Emily Yu <emily.y@pinecone.io>
2024-06-13 16:40:43 -0400

    Condense control code

--- pinecone_sdk/src/control/mod.rs
+++
@@ -1,8 +0,0 @@
-/// Module for creating an index.
-pub mod create_index;
-
-/// Module for listing all indexes.
-pub mod list_indexes;
-
-pub use openapi::models::create_index_request::Metric;
-pub use openapi::models::serverless_spec::Cloud;
