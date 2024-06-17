b5a66b5abbc397d5991a8302bf7d41484efdd4c1
Emily Yu <35238826+emily-emily@users.noreply.github.com>
2024-06-17 09:51:37 -0400

    Implement describe_index (#8)
    ## Problem
    
    `describe_index` was not implemented.
    
    ## Solution
    
    This change implements `describe_index` and updates some documentation.
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    - [ ] Infrastructure change (CI configs, etc)
    - [x] Non-code change (docs, etc)
    - [ ] None of the above: (explain here)
    
    ## Test Plan
    
    Existing test cases pass, and one additional test case was added for
    `describe_index` that passes.

--- pinecone_sdk/src/control.rs
+++ pinecone_sdk/src/control.rs
@@ -9,7 +9,40 @@ pub use openapi::models::create_index_request::Metric;
 pub use openapi::models::serverless_spec::Cloud;
 
 impl PineconeClient {
-    /// Creates serverless index
+    /// Creates a serverless index.
+    ///
+    /// ### Arguments
+    /// * `name: &str` - Name of the index to create.
+    /// * `dimension: u32` - Dimension of the vectors to be inserted in the index.
+    /// * `metric: Metric` - The distance metric to be used for similarity search.
+    /// * `cloud: Cloud` - The public cloud where you would like your index hosted.
+    /// * `region: &str` - The region where you would like your index to be created.
+    ///
+    /// ### Return
+    /// * `Result<IndexModel, PineconeError>`
+    ///
+    /// ### Example
+    /// ```no_run
+    /// use pinecone_sdk::pinecone::PineconeClient;
+    /// use pinecone_sdk::utils::errors::PineconeError;
+    /// use pinecone_sdk::control::{Metric, Cloud};
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), PineconeError>{
+    /// let pinecone = PineconeClient::new(None, None, None, None).unwrap();
+    ///
+    /// // Create an index.
+    /// let create_index_request = pinecone.create_serverless_index(
+    ///     "create-index", // Name of the index
+    ///     10, // Dimension of the vectors
+    ///     Metric::Cosine, // Distance metric
+    ///     Cloud::Aws, // Cloud provider
+    ///     "us-east-1" // Region
+    /// ).await.unwrap();
+    ///
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn create_serverless_index(
         &self,
         name: &str,
@@ -39,37 +72,64 @@ impl PineconeClient {
             Err(e) => Err(PineconeError::CreateIndexError { openapi_error: e }),
         }
     }
+
+    /// Describes an index.
+    ///
+    /// ### Arguments
+    /// * `name: &str` - Name of the index to describe.
+    ///
+    /// ### Return
+    /// * `Result<IndexModel, PineconeError>`
+    ///
+    /// ### Example
+    /// ```no_run
+    /// use pinecone_sdk::pinecone::PineconeClient;
+    /// use pinecone_sdk::utils::errors::PineconeError;
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), PineconeError>{
+    /// let pinecone = PineconeClient::new(None, None, None, None).unwrap();
+    ///
+    /// // Describe an index in the project.
+    /// let index = pinecone.describe_index("index-name").await.unwrap();
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub async fn describe_index(&self, name: &str) -> Result<IndexModel, PineconeError> {
+        match manage_indexes_api::describe_index(&self.openapi_config(), name).await {
+            Ok(index) => Ok(index),
+            Err(e) => Err(PineconeError::DescribeIndexError {
+                name: name.to_string(),
+                openapi_error: e,
+            }),
+        }
+    }
+
     /// Lists all indexes.
     ///
     /// The results include a description of all indexes in your project, including the
     /// index name, dimension, metric, status, and spec.
     ///
-    /// :return: Returns an `IndexList` object, which is iterable and contains a
-    ///     list of `IndexDescription` objects. It also has a convenience method `names()`
-    ///     which returns a list of index names.
+    /// ### Return
+    /// * `Result<IndexList, PineconeError>`
     ///
     /// ### Example
+    /// ```no_run
+    /// use pinecone_sdk::pinecone::PineconeClient;
+    /// use pinecone_sdk::utils::errors::PineconeError;
     ///
-    /// ```
-    /// # use pinecone_sdk::pinecone::PineconeClient;
-    /// # use pinecone_sdk::utils::errors::PineconeError;
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), PineconeError>{
-    /// # // Create a Pinecone client with the API key and controller host.
-    /// # let pinecone = PineconeClient::new(None, None, None, None).unwrap();
-    /// #
+    /// let pinecone = PineconeClient::new(None, None, None, None).unwrap();
+    ///
     /// // List all indexes in the project.
-    /// let index_list = pinecone.list_indexes();
+    /// let index_list = pinecone.list_indexes().await.unwrap();
     /// # Ok(())
     /// # }
     /// ```
-
     pub async fn list_indexes(&self) -> Result<IndexList, PineconeError> {
         match manage_indexes_api::list_indexes(&self.openapi_config()).await {
-            Ok(response) => {
-                println!("{:?}", response);
-                Ok(response)
-            }
+            Ok(index_list) => Ok(index_list),
             Err(e) => Err(PineconeError::ListIndexesError { openapi_error: e }),
         }
     }
@@ -80,17 +140,18 @@ impl PineconeClient {
     /// * name: &str - The name of the index to be deleted.
     ///
     /// ### Return
-    /// * Returns a `Result<(), PineconeError>` object. 
+    /// * Returns a `Result<(), PineconeError>` object.
     ///
     /// ### Example
     /// ```no_run
-    /// # use pinecone_sdk::pinecone::PineconeClient;
-    /// # use pinecone_sdk::control::{Cloud, Metric};
-    /// # use pinecone_sdk::utils::errors::PineconeError;
+    /// use pinecone_sdk::pinecone::PineconeClient;
+    /// use pinecone_sdk::control::{Cloud, Metric};
+    /// use pinecone_sdk::utils::errors::PineconeError;
+    ///
     /// # #[tokio::main]
     /// # async fn main() -> Result<(), PineconeError>{
-    /// # let pinecone = PineconeClient::new(None, None, None, None).unwrap();
-    /// # let _ = pinecone.create_serverless_index("index-name", 2, Metric::Euclidean, Cloud::Aws, "us-west-2").await;
+    /// let pinecone = PineconeClient::new(None, None, None, None).unwrap();
+    ///
     /// let response = pinecone.delete_index("index-name").await;
     /// # Ok(())
     /// # }
@@ -110,11 +171,11 @@ impl PineconeClient {
 mod tests {
     use super::*;
     use mockito::mock;
-    use openapi::models;
+    use openapi::models::{self, IndexList};
     use tokio;
 
     #[tokio::test]
-    async fn test_create_serverless_index() {
+    async fn test_create_serverless_index() -> Result<(), PineconeError> {
         let _m = mock("POST", "/indexes")
             .with_status(201)
             .with_header("content-type", "application/json")
@@ -150,24 +211,25 @@ mod tests {
         let create_index_request = pinecone
             .unwrap()
             .create_serverless_index("index_name", 10, Metric::Cosine, Cloud::Aws, "us-east-1")
-            .await;
-        assert!(create_index_request.is_ok());
+            .await
+            .expect("Failed to create index");
 
-        let create_index_req = create_index_request.unwrap();
-        assert_eq!(create_index_req.name, "index_name");
-        assert_eq!(create_index_req.dimension, 10);
+        assert_eq!(create_index_request.name, "index_name");
+        assert_eq!(create_index_request.dimension, 10);
         assert_eq!(
-            create_index_req.metric,
+            create_index_request.metric,
             openapi::models::index_model::Metric::Euclidean
         );
 
-        let spec = create_index_req.spec.serverless.unwrap();
+        let spec = create_index_request.spec.serverless.unwrap();
         assert_eq!(spec.cloud, openapi::models::serverless_spec::Cloud::Aws);
         assert_eq!(spec.region, "us-east-1");
+
+        Ok(())
     }
 
     #[tokio::test]
-    async fn test_create_serverless_index_defaults() {
+    async fn test_create_serverless_index_defaults() -> Result<(), PineconeError> {
         let _m = mock("POST", "/indexes")
             .with_status(201)
             .with_header("content-type", "application/json")
@@ -209,20 +271,82 @@ mod tests {
                 Default::default(),
                 "us-east-1",
             )
-            .await;
-        assert!(create_index_request.is_ok());
+            .await
+            .expect("Failed to create index");
 
-        let create_index_req = create_index_request.unwrap();
-        assert_eq!(create_index_req.name, "index_name");
-        assert_eq!(create_index_req.dimension, 10);
+        assert_eq!(create_index_request.name, "index_name");
+        assert_eq!(create_index_request.dimension, 10);
         assert_eq!(
-            create_index_req.metric,
+            create_index_request.metric,
             openapi::models::index_model::Metric::Cosine
         );
 
-        let spec = create_index_req.spec.serverless.unwrap();
+        let spec = create_index_request.spec.serverless.unwrap();
         assert_eq!(spec.cloud, openapi::models::serverless_spec::Cloud::Gcp);
         assert_eq!(spec.region, "us-east-1");
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_describe_index() -> Result<(), PineconeError> {
+        // Create a mock server
+        let _m = mock("GET", "/indexes/serverless-index")
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(
+                r#"
+                {
+                    "name": "serverless-index",
+                    "metric": "cosine",
+                    "dimension": 1536,
+                    "status": {
+                       "ready": true,
+                       "state": "Ready"
+                    },
+                    "host": "serverless-index-4zo0ijk.svc.us-east1-aws.pinecone.io",
+                    "spec": {
+                       "serverless": {
+                          "region": "us-east-1",
+                          "cloud": "aws"
+                       }
+                    }
+                 }
+            "#,
+            )
+            .create();
+
+        // Construct Pinecone instance with the mock server URL
+        let api_key = "test_api_key".to_string();
+        let pinecone = PineconeClient::new(Some(api_key), Some(mockito::server_url()), None, None)
+            .expect("Failed to create Pinecone instance");
+
+        // Call describe_index and verify the result
+        let index = pinecone
+            .describe_index("serverless-index")
+            .await
+            .expect("Failed to describe index");
+
+        let expected = IndexModel {
+            name: "serverless-index".to_string(),
+            metric: openapi::models::index_model::Metric::Cosine,
+            dimension: 1536,
+            status: Box::new(openapi::models::IndexModelStatus {
+                ready: true,
+                state: openapi::models::index_model_status::State::Ready,
+            }),
+            host: "serverless-index-4zo0ijk.svc.us-east1-aws.pinecone.io".to_string(),
+            spec: Box::new(models::IndexModelSpec {
+                serverless: Some(Box::new(models::ServerlessSpec {
+                    cloud: openapi::models::serverless_spec::Cloud::Aws,
+                    region: "us-east-1".to_string(),
+                })),
+                pod: None,
+            }),
+        };
+        assert_eq!(index, expected);
+
+        Ok(())
     }
 
     #[tokio::test]
