947933d732d05993f0cc833d5c09b7823e31b2c5
Erica Wang <46633719+ericapywang@users.noreply.github.com>
2024-07-10 13:05:14 -0400

    Restructure PineconeError (#21)
    ## Problem
    
    `PineconeError` could use a lot of revisions -- some of the errors are
    operation-specific and aren't very informative. Some of the inner
    information, such as the error and status code, may be difficult to
    parse given that they are stored inside the generated error which is
    then stored inside the error.
    
    ## Solution
    
    I changed the error types to be more generalized and not
    operation-specific. I also updated each of the values in `PineconeError`
    to have a `status` (if applicable) and `message` (which stores the error
    message) fields. This allows the user to more easily extract the
    information if needed, allows us to customize the error message a bit
    more, and makes the different enum values more consistent. I also added
    code to extract these values from the originally generated error.
    
    ## Type of Change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    - [ ] Infrastructure change (CI configs, etc)
    - [ ] Non-code change (docs, etc)
    - [ ] None of the above: (explain here)
    
    ## Test Plan
    
    The test cases should pass

--- pinecone_sdk/src/utils/errors.rs
+++ pinecone_sdk/src/utils/errors.rs
@@ -1,119 +1,262 @@
-use openapi::apis::{
-    manage_indexes_api::{
-        ConfigureIndexError, CreateCollectionError, CreateIndexError, DeleteCollectionError,
-        DeleteIndexError, DescribeIndexError, ListCollectionsError, ListIndexesError,
-    },
-    Error as OpenAPIError,
-};
-use snafu::prelude::*;
+use openapi::apis::{Error as OpenApiError, ResponseContent};
+use reqwest::{self, StatusCode};
 
 /// PineconeError is the error type for all Pinecone SDK errors.
-#[derive(Debug, Snafu)]
+#[derive(Debug)]
 pub enum PineconeError {
     /// APIKeyMissingError: API key is not provided as an argument nor in the environment variable `PINECONE_API_KEY`.
-    #[snafu(display("API key missing."))]
-    APIKeyMissingError,
-
-    /// ConfigureIndexError: Failed to configure an index.
-    #[snafu(display("Failed to configure index '{}'.", name))]
-    ConfigureIndexError {
-        /// name: Index name.
-        name: String,
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<ConfigureIndexError>,
-    },
-
-    /// CreateCollectionError: Failed to create a collection.
-    #[snafu(display("Failed to create collection '{}'.", name))]
-    CreateCollectionError {
-        /// name: Collection name.
-        name: String,
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<CreateCollectionError>,
-    },
-
-    /// CreateIndexError: Failed to create an index.
-    #[snafu(display("Failed to create an index."))]
-    CreateIndexError {
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<CreateIndexError>,
-    },
-
-    /// DeleteCollectionError: Failed to delete an index.
-    #[snafu(display("Failed to delete collection '{}'.", name))]
-    DeleteCollectionError {
-        /// name: Index name.
-        name: String,
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<DeleteCollectionError>,
-    },
-
-    /// DeleteIndexError: Failed to delete an index.
-    #[snafu(display("Failed to delete index '{}'.", name))]
-    DeleteIndexError {
-        /// name: Index name.
-        name: String,
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<DeleteIndexError>,
-    },
-
-    /// DescribeIndexError: Failed to describe an index.
-    #[snafu(display("Failed to describe the index '{}'.", name))]
-    DescribeIndexError {
-        /// name: Index name.
-        name: String,
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<DescribeIndexError>,
-    },
-
-    /// InvalidCloudError: Provided cloud is not valid.
-    #[snafu(display("Invalid cloud '{}'.", cloud))]
-    InvalidCloudError {
-        /// cloud: Cloud name.
-        cloud: String,
+    APIKeyMissingError {
+        /// Error message.
+        message: String,
     },
 
     /// InvalidHeadersError: Provided headers are not valid. Expects JSON.
-    #[snafu(display("Failed to parse headers: {}", json_error))]
     InvalidHeadersError {
-        /// json_error: Error object for JSON parsing error.
-        json_error: serde_json::Error,
+        /// Error message.
+        message: String,
     },
-
-    /// InvalidMetricError: Provided metric is not valid.
-    #[snafu(display("Invalid metric '{}'.", metric))]
-    InvalidMetricError {
-        /// metric: Metric name.
-        metric: String,
-    },
-
-    /// ListCollectionsError: Failed to list indexes.
-    #[snafu(display("Failed to list collections."))]
-    ListCollectionsError {
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<ListCollectionsError>,
-    },
-
-    /// ListIndexesError: Failed to list indexes.
-    #[snafu(display("Failed to list indexes."))]
-    ListIndexesError {
-        /// openapi_error: Error object for OpenAPI error.
-        openapi_error: OpenAPIError<ListIndexesError>,
-    },
-
-    /// MissingDimensionError: Index dimension is missing.
-    #[snafu(display("Dimension missing."))]
-    MissingDimensionError,
-
-    /// MissingNameError: Index name is missing.
-    #[snafu(display("Index name missing."))]
-    MissingNameError,
-
-    /// MissingSpecError: Index spec is missing.
-    #[snafu(display("Spec missing."))]
-    MissingSpecError,
 
     /// TimeoutError: Request timed out.
-    #[snafu(display("Request timed out."))]
-    TimeoutError,
+    TimeoutError {
+        /// Error message.
+        message: String,
+    },
+
+    /// ReqwestError: Error caused by Reqwest
+    ReqwestError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// SerdeError: Error caused by Serde
+    SerdeError {
+        /// Error message.
+        message: String,
+    },
+
+    /// IoError: Error caused by IO
+    IoError {
+        /// Error message.
+        message: String,
+    },
+
+    /// BadRequestError: Bad request. The request body included invalid request parameters
+    BadRequestError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// UnauthorizedError: Unauthorized. Possibly caused by invalid API key
+    UnauthorizedError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// PodQuotaExceededError: Pod quota exceeded
+    PodQuotaExceededError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// CollectionsQuotaExceededError: Collections quota exceeded
+    CollectionsQuotaExceededError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// InvalidCloudError: Provided cloud is not valid.
+    InvalidCloudError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// InvalidRegionError: Provided region is not valid.
+    InvalidRegionError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// CollectionNotFoundError: Collection of given name does not exist
+    CollectionNotFoundError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String
+    },
+
+    /// IndexNotFoundError: Index of given name does not exist
+    IndexNotFoundError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// IndexAlreadyExistsError: Index of given name already exists
+    IndexAlreadyExistsError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// CollectionAlreadyExistsError: Collection of given name already exists
+    CollectionAlreadyExistsError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// Unprocessable entity error: The request body could not be deserialized
+    UnprocessableEntityError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// PendingCollectionError: There is a pending collection created from this index
+    PendingCollectionError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+
+    /// InternalServerError: Internal server error
+    InternalServerError {
+        /// HTTP status code.
+        status: StatusCode,
+        /// Error message.
+        message: String,
+    },
+}
+
+// Implement the conversion from OpenApiError to PineconeError for CreateIndexError.
+impl<T> From<(OpenApiError<T>, String)> for PineconeError {
+    fn from((error, message): (OpenApiError<T>, String)) -> Self {
+        err_handler(error, message)
+    }
+}
+
+// Helper function to extract status/error message
+fn err_handler<T>(e: OpenApiError<T>, message: String) -> PineconeError {
+    match e {
+        OpenApiError::Reqwest(inner) => PineconeError::ReqwestError {
+            status: inner.status().unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
+            message: inner.to_string(),
+        },
+        OpenApiError::Serde(inner) => PineconeError::SerdeError {
+            message: inner.to_string(),
+        },
+        OpenApiError::Io(inner) => PineconeError::IoError {
+            message: inner.to_string(),
+        },
+        OpenApiError::ResponseError(inner) => handle_response_error(inner, message),
+    }
+}
+
+// Helper function to handle response errors
+fn handle_response_error<T>(e: ResponseContent<T>, message: String) -> PineconeError {
+    let err_message = e.content;
+    let status = e.status;
+    let message = format!("{message}: {err_message}");
+
+    match status {
+        StatusCode::BAD_REQUEST => PineconeError::BadRequestError { status, message },
+        StatusCode::UNAUTHORIZED => PineconeError::UnauthorizedError { status, message },
+        StatusCode::FORBIDDEN => parse_quota_exceeded_error(message),
+        StatusCode::NOT_FOUND => parse_not_found_error(message),
+        StatusCode::CONFLICT => parse_conflict_error(message),
+        StatusCode::PRECONDITION_FAILED => {
+            PineconeError::PendingCollectionError { status, message }
+        }
+        StatusCode::UNPROCESSABLE_ENTITY => {
+            PineconeError::UnprocessableEntityError { status, message }
+        }
+        StatusCode::INTERNAL_SERVER_ERROR => PineconeError::InternalServerError { status, message },
+        _ => PineconeError::ReqwestError { status, message },
+    }
+}
+
+fn parse_not_found_error(message: String) -> PineconeError {
+    if message.contains("Index") {
+        PineconeError::IndexNotFoundError {
+            status: StatusCode::NOT_FOUND,
+            message,
+        }
+    } else if message.contains("Collection") {
+        PineconeError::CollectionNotFoundError {
+            status: StatusCode::NOT_FOUND,
+            message,
+        }
+    } else if message.contains("region") {
+        PineconeError::InvalidRegionError {
+            status: StatusCode::NOT_FOUND,
+            message,
+        }
+    } else if message.contains("cloud") {
+        PineconeError::InvalidCloudError {
+            status: StatusCode::NOT_FOUND,
+            message,
+        }
+    } else {
+        PineconeError::InternalServerError {
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+            message,
+        }
+    }
+}
+
+fn parse_conflict_error(message: String) -> PineconeError {
+    if message.contains("index") {
+        PineconeError::IndexAlreadyExistsError {
+            status: StatusCode::CONFLICT,
+            message,
+        }
+    } else if message.contains("collection") {
+        PineconeError::CollectionAlreadyExistsError {
+            status: StatusCode::CONFLICT,
+            message,
+        }
+    } else {
+        PineconeError::InternalServerError {
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+            message,
+        }
+    }
+}
+
+fn parse_quota_exceeded_error(message: String) -> PineconeError {
+    if message.contains("index") {
+        PineconeError::PodQuotaExceededError {
+            status: StatusCode::FORBIDDEN,
+            message,
+        }
+    } else if message.contains("Collection") {
+        PineconeError::CollectionsQuotaExceededError {
+            status: StatusCode::FORBIDDEN,
+            message,
+        }
+    } else {
+        PineconeError::InternalServerError {
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+            message,
+        }
+    }
 }
