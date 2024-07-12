b722426dca3ddc5ecf35ef4885edae79b22b3533
Silas Smith <163026730+ssmith-pc@users.noreply.github.com>
2024-07-12 13:31:26 -0700

    try it out

--- pinecone_sdk/src/utils/errors.rs
+++ pinecone_sdk/src/utils/errors.rs
@@ -4,6 +4,13 @@ use reqwest::{self, StatusCode};
 /// PineconeError is the error type for all Pinecone SDK errors.
 #[derive(Debug)]
 pub enum PineconeError {
+    /// UnknownResponseError: Unknown response error.
+    UnknownResponseError {
+        /// status code
+        status: StatusCode,
+        /// message
+        message: String,
+    },
     /// APIKeyMissingError: API key is not provided as an argument nor in the environment variable `PINECONE_API_KEY`.
     APIKeyMissingError {
         /// Error message.
@@ -24,16 +31,14 @@ pub enum PineconeError {
 
     /// ReqwestError: Error caused by Reqwest
     ReqwestError {
-        /// HTTP status code.
-        status: StatusCode,
-        /// Error message.
-        message: String,
+        /// Source error
+        source: reqwest::Error,
     },
 
     /// SerdeError: Error caused by Serde
     SerdeError {
-        /// Error message.
-        message: String,
+        /// Source of the error.
+        source: serde_json::Error,
     },
 
     /// IoError: Error caused by IO
@@ -95,7 +100,7 @@ pub enum PineconeError {
         /// HTTP status code.
         status: StatusCode,
         /// Error message.
-        message: String
+        message: String,
     },
 
     /// IndexNotFoundError: Index of given name does not exist
@@ -157,13 +162,8 @@ impl<T> From<(OpenApiError<T>, String)> for PineconeError {
 // Helper function to extract status/error message
 fn err_handler<T>(e: OpenApiError<T>, message: String) -> PineconeError {
     match e {
-        OpenApiError::Reqwest(inner) => PineconeError::ReqwestError {
-            status: inner.status().unwrap_or(StatusCode::INTERNAL_SERVER_ERROR),
-            message: inner.to_string(),
-        },
-        OpenApiError::Serde(inner) => PineconeError::SerdeError {
-            message: inner.to_string(),
-        },
+        OpenApiError::Reqwest(inner) => PineconeError::ReqwestError { source: inner },
+        OpenApiError::Serde(inner) => PineconeError::SerdeError { source: inner },
         OpenApiError::Io(inner) => PineconeError::IoError {
             message: inner.to_string(),
         },
@@ -190,7 +190,10 @@ fn handle_response_error<T>(e: ResponseContent<T>, message: String) -> PineconeE
             PineconeError::UnprocessableEntityError { status, message }
         }
         StatusCode::INTERNAL_SERVER_ERROR => PineconeError::InternalServerError { status, message },
-        _ => PineconeError::ReqwestError { status, message },
+        _ => PineconeError::UnknownResponseError {
+            status,
+            message: err_message,
+        },
     }
 }
 
@@ -257,6 +260,198 @@ fn parse_quota_exceeded_error(message: String) -> PineconeError {
         PineconeError::InternalServerError {
             status: StatusCode::INTERNAL_SERVER_ERROR,
             message,
+        }
+    }
+}
+
+impl std::fmt::Display for PineconeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PineconeError::UnknownResponseError { status, message } => {
+                write!(
+                    f,
+                    "Unknown response error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::CollectionAlreadyExistsError { status, message } => {
+                write!(
+                    f,
+                    "Collection already exists error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::UnprocessableEntityError { status, message } => {
+                write!(
+                    f,
+                    "Unprocessable entity error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::PendingCollectionError { status, message } => {
+                write!(
+                    f,
+                    "Pending collection error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::InternalServerError { status, message } => {
+                write!(
+                    f,
+                    "Internal server error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::ReqwestError { source } => {
+                write!(f, "Reqwest error: {}", source.to_string())
+            }
+            PineconeError::SerdeError { source } => {
+                write!(f, "Serde error: {}", source.to_string())
+            }
+            PineconeError::IoError { message } => {
+                write!(f, "IO error: {}", message)
+            }
+            PineconeError::BadRequestError { status, message } => {
+                write!(
+                    f,
+                    "Bad request error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::UnauthorizedError { status, message } => {
+                write!(
+                    f,
+                    "Unauthorized error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::PodQuotaExceededError { status, message } => {
+                write!(
+                    f,
+                    "Pod quota exceeded error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::CollectionsQuotaExceededError { status, message } => {
+                write!(
+                    f,
+                    "Collections quota exceeded error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::InvalidCloudError { status, message } => {
+                write!(
+                    f,
+                    "Invalid cloud error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::InvalidRegionError { status, message } => {
+                write!(
+                    f,
+                    "Invalid region error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::CollectionNotFoundError { status, message } => {
+                write!(
+                    f,
+                    "Collection not found error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::IndexNotFoundError { status, message } => {
+                write!(
+                    f,
+                    "Index not found error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::IndexAlreadyExistsError { status, message } => {
+                write!(
+                    f,
+                    "Index already exists error: status: {}, message: {}",
+                    status, message
+                )
+            }
+            PineconeError::APIKeyMissingError { message } => {
+                write!(f, "API key missing error: {}", message)
+            }
+            PineconeError::InvalidHeadersError { message } => {
+                write!(f, "Invalid headers error: message: {}", message)
+            }
+            PineconeError::TimeoutError { message } => {
+                write!(f, "Timeout error: message: {}", message)
+            }
+        }
+    }
+}
+
+impl std::error::Error for PineconeError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            PineconeError::UnknownResponseError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::ReqwestError { source } => Some(source),
+            PineconeError::SerdeError { source } => Some(source),
+            PineconeError::IoError { message: _ } => None,
+            PineconeError::BadRequestError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::UnauthorizedError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::PodQuotaExceededError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::CollectionsQuotaExceededError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::InvalidCloudError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::InvalidRegionError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::CollectionNotFoundError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::IndexNotFoundError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::IndexAlreadyExistsError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::CollectionAlreadyExistsError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::UnprocessableEntityError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::PendingCollectionError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::InternalServerError {
+                status: _,
+                message: _,
+            } => None,
+            PineconeError::APIKeyMissingError { message: _ } => None,
+            PineconeError::InvalidHeadersError { message: _ } => None,
+            PineconeError::TimeoutError { message: _ } => None,
         }
     }
 }
