22428916890342a656492e08d1d2ee60939ce152
Emily Yu <emily.y@pinecone.io>
2024-07-15 19:11:00 -0400

    Update bash scripts

--- codegen/build-proto.sh
+++ codegen/build-proto.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# pushd codegen/apis
-# 	just build
-# popd
-
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 outdir="protos"
-rm -rf $outdir
-mkdir $outdir
 
-# export OUT_DIR as pwd/../outdir with the parent!!!
-export OUT_DIR=$(pwd)/$outdir
+pushd codegen/apis
+	just build
+popd
+
+export OUT_DIR=$SCRIPT_DIR/../$outdir
 
 pushd codegen/proto_build
 	cargo run
